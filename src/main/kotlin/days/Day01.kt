223d5c81e60e75d3ef18f881d84d365fc9b0e58a
Ben <benrflynn@yahoo.co.uk>
2022-12-01 23:31:08 +0000

    Updated Day 1. Created files for Day 2.

--- src/main/kotlin/days/Day01.kt
+++ src/main/kotlin/days/Day01.kt
@@ -1,33 +1,21 @@
 package days
 
 class Day01 : Day(1) {
+
+    private var max = 0
+    private val maxArray: MutableList<Int> = mutableListOf()
+
+    private val calories = inputList.forEach {
+        if(it == "" || inputList.indexOf(it) == inputList.size) {
+            maxArray.add(max)
+            max = 0
+        } else max += it.toInt()
+    }
     override fun part1() : Any {
-        var max = 0
-        val maxArray: MutableList<Int> = mutableListOf()
-        inputList.forEach {
-            if(it == "" || inputList.indexOf(it) == inputList.size) {
-                maxArray.add(max)
-                max = 0
-            } else max += it.toInt()
-        }
         return maxArray.maxOf { it }
     }
 
     override fun part2() : Any {
-        var max = 0
-        val maxArray: MutableList<Int> = mutableListOf()
-
-        inputList.forEach {
-            if(it == "" || inputList.indexOf(it) == inputList.size-1) {
-                maxArray.add(max)
-                max = 0
-            } else max += it.toInt()
-        }
-        val max1 = maxArray.maxOf { it }
-        maxArray.removeIf { it == max1 }
-        val max2 = maxArray.maxOf { it }
-        maxArray.removeIf { it == max2 }
-
-        return max1 + max2 + maxArray.maxOf { it }
+        return maxArray.sorted().takeLast(3).sum()
     }
 }
