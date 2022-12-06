23ab48f410d301d1cdda4664a90fd4b3d4bde066
Ben <benrflynn@yahoo.co.uk>
2022-12-06 14:08:33 +0000

    Updated Day 6 to remove unnecessary looping element, didn't realise input would be one element.

--- src/main/kotlin/days/Day06.kt
+++ src/main/kotlin/days/Day06.kt
@@ -1,32 +1,28 @@
 package days
 
 class Day06 : Day(6) {
-    override fun part1() : Any {
-        inputList.forEach {
-            val window = it.windowed(4, 1)
-            window.forEachIndexed { index, s ->
-                if(uniqueChars(s, 1)) {
-                    return index+4
-                }
+    override fun part1(): Any {
+        val window = inputList.first().windowed(4, 1)
+        window.forEachIndexed { index, s ->
+            if (uniqueChars(s, 1)) {
+                return index + 4
             }
         }
         return 0
     }
 
-    override fun part2() : Any {
-        inputList.forEach {
-            val window = it.windowed(14, 1)
-            window.forEachIndexed { index2, s ->
-                if(uniqueChars(s, 2)) {
-                    return index2+14
-                }
+    override fun part2(): Any {
+        val window = inputList.first().windowed(14, 1)
+        window.forEachIndexed { index2, s ->
+            if (uniqueChars(s, 2)) {
+                return index2 + 14
             }
         }
         return 0
     }
 
-    private fun uniqueChars(s: String, part: Int) : Boolean {
-        return if(part == 1) s.toSet().size == 4
+    private fun uniqueChars(s: String, part: Int): Boolean {
+        return if (part == 1) s.toSet().size == 4
         else s.toSet().size == 14
     }
 }
